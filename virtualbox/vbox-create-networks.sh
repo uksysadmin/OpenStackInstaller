5f0799752ffb9a642e82713f0f4064e7e514c205
Kevin Jackson <kevin@linuxservices.co.uk>
2012-10-10 18:03:10 +0100

    Added 3rd host-only networking interface

--- virtualbox/vbox-create-networks.sh
+++ virtualbox/vbox-create-networks.sh
@@ -7,3 +7,7 @@ VBoxManage hostonlyif ipconfig vboxnet0 --ip 172.16.0.254 --netmask 255.255.0.0
 # Private Network vboxnet1 (10.0.0.0/8)
 VBoxManage hostonlyif create
 VBoxManage hostonlyif ipconfig vboxnet1 --ip 10.0.0.254 --netmask 255.0.0.0
+
+# Private Network vboxnet2 (192.168.57.0/8)
+VBoxManage hostonlyif create
+VBoxManage hostonlyif ipconfig vboxnet1 --ip 192.168.57.1 --netmask 255.255.255.0
