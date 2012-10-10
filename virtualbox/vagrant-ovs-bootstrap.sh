bb466cf63b4425d5c20ffdfbfb6da329af2c5b8a
uksysadmin <kevin@linuxservices.co.uk>
2012-10-10 12:57:09 +0100

    Removed autoboot of OVS VM; OVS now set on openstack VM with an extra interface

---
+++ virtualbox/vagrant-ovs-bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Ensure git is installed
+sudo apt-get update
+sudo apt-get -y install git
+
+# git clone OpenStackInstaller
+git clone https://github.com/uksysadmin/OpenStackInstaller.git
+cd OpenStackInstaller
+git checkout folsom
+./install-ovs.sh
