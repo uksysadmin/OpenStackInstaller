f9ef923ed3de1b278f512bcd3adf81d359f302b1
uksysadmin <kevin@linuxservices.co.uk>
2012-10-12 15:08:42 +0100

    Attempting to match the docs more

--- folsom-quantum-createnetwork.sh
+++ folsom-quantum-createnetwork.sh
@@ -24,19 +24,23 @@ TENANT_ID=$(keystone tenant-list | awk '/ admin / { print $2}')
 TENANT_NET_ID=$(get_id quantum net-create --tenant_id $TENANT_ID demo-net)
 
 # Create the private network range in this named network
-SUBNET_ID=$(get_id quantum subnet-create --tenant_id ${TENANT_ID} --ip_version 4 ${TENANT_NET_ID} ${PRIV_CIDR} --gateway_ip ${PRIV_GW})
+# SUBNET_ID=$(get_id quantum subnet-create --tenant_id ${TENANT_ID} --ip_version 4 ${TENANT_NET_ID} ${PRIV_CIDR} --gateway_ip ${PRIV_GW})
+SUBNET_ID=$(get_id quantum subnet-create --tenant_id ${TENANT_ID} demo-net ${PRIV_CIDR} )
 
-# Create a private router for this tenant
-ROUTER_ID=$(get_id quantum router-create --tenant_id ${TENANT_ID} provider-router)
 
-# Create an interface on the router
-quantum router-interface-add ${ROUTER_ID} ${SUBNET_ID}
 
 
 ###########################################################
 #
 # Create External Network (ext_net)
 #
+# Create a private router for this tenant
+ROUTER_ID=$(get_id quantum router-create --tenant_id ${TENANT_ID} provider-router)
+
+# Create an interface on the router
+quantum router-interface-add ${ROUTER_ID} ${SUBNET_ID}
+
+# Create external network
 EXT_NET_ID=$(get_id quantum net-create ext_net -- --router:external=True)
 
 # Create Floating IP Range
