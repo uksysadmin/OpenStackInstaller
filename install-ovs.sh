1b8f17377497fb177876b805567a9bb60cd8868f
uksysadmin <kevin@linuxservices.co.uk>
2012-10-31 14:11:27 +0000

    quantum fixes and flexibility in configs

--- install-ovs.sh
+++ install-ovs.sh
@@ -17,16 +17,16 @@ ovs_install() {
 
 ovs_configure() {
 	sudo ovs-vsctl add-br br-int
-	sudo ovs-vsctl add-br br-ex
-	sudo ovs-vsctl br-set-external-id br-ex bridge-id br-ex
-	sudo ovs-vsctl add-port br-ex $OVS_EXTERNAL_INTERFACE
+	sudo ovs-vsctl add-br ${EXT_BRIDGE}
+	sudo ovs-vsctl br-set-external-id ${EXT_BRIDGE} bridge-id ${EXT_BRIDGE}
+	sudo ovs-vsctl add-port ${EXT_BRIDGE} $OVS_EXTERNAL_INTERFACE
 }
 
 # Main
 ovs_install
 ovs_configure
 
-# Configure br-ex to reach public network :
-sudo ip addr flush dev br-ex
-sudo ip addr add ${FLOAT_GATEWAY}/255.255.255.0 dev br-ex
-sudo ip link set br-ex up
+# Configure ${EXT_BRIDGE} to reach public network :
+sudo ip addr flush dev ${EXT_BRIDGE}
+sudo ip addr add ${FLOAT_GATEWAY}/255.255.255.0 dev ${EXT_BRIDGE}
+sudo ip link set ${EXT_BRIDGE} up
