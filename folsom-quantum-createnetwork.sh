9bfc560702bbd5bab8cc419a34cce57f3cf7aff7
uksysadmin <kevin@linuxservices.co.uk>
2012-11-01 11:44:42 +0000

    Troubleshooting my own setup - sorry folks - probably break yours until I make more flexible

--- folsom-quantum-createnetwork.sh
+++ folsom-quantum-createnetwork.sh
@@ -24,9 +24,13 @@ TENANT_ID=$(keystone tenant-list | awk '/ admin / { print $2}')
 TENANT_NET_ID=$(get_id quantum net-create --tenant_id $TENANT_ID ${TENANT_NETWORK_NAME})
 
 # Create the private network range in this named network
-# SUBNET_ID=$(get_id quantum subnet-create --tenant_id ${TENANT_ID} --ip_version 4 ${TENANT_NET_ID} ${PRIV_CIDR} --gateway_ip ${PRIV_GW})
 SUBNET_ID=$(get_id quantum subnet-create --tenant_id ${TENANT_ID} --ip_version 4 ${TENANT_NET_ID} ${PRIV_CIDR} --gateway_ip ${PRIV_GW})
 
+# Create a private router for this tenant
+ROUTER_ID=$(get_id quantum router-create --tenant_id ${TENANT_ID} provider-router)
+
+# Create an interface on the router
+quantum router-interface-add ${ROUTER_ID} ${SUBNET_ID}
 
 
 
@@ -34,20 +38,15 @@ SUBNET_ID=$(get_id quantum subnet-create --tenant_id ${TENANT_ID} --ip_version 4
 #
 # Create External Network (${EXTERNAL_NETWORK_NAME})
 #
-# Create a private router for this tenant
-ROUTER_ID=$(get_id quantum router-create --tenant_id ${TENANT_ID} provider-router)
-
-# Create an interface on the router
-quantum router-interface-add ${ROUTER_ID} ${SUBNET_ID}
 
 # Create external network
-EXT_NET_ID=$(get_id quantum net-create ${EXTERNAL_NETWORK_NAME} -- --router:external=True)
+#EXT_NET_ID=$(get_id quantum net-create ${EXTERNAL_NETWORK_NAME} -- --router:external=True)
 
 # Create Floating IP Range
-quantum subnet-create --ip_version 4 --allocation-pool start=${FLOAT_START},end=${FLOAT_END} --gateway ${FLOAT_GATEWAY} ${EXT_NET_ID} ${EXT_CIDR} -- --enable_dhcp=False
+#quantum subnet-create --ip_version 4 --allocation-pool start=${FLOAT_START},end=${FLOAT_END} --gateway ${FLOAT_GATEWAY} ${EXT_NET_ID} ${EXT_CIDR} -- --enable_dhcp=False
 
 # Set the gateway for the router
-quantum router-gateway-set ${ROUTER_ID} ${EXT_NET_ID}
+#quantum router-gateway-set ${ROUTER_ID} ${EXT_NET_ID}
 
 # Create floating IP for ${EXTERNAL_NETWORK_NAME}
-quantum floatingip-create ${EXTERNAL_NETWORK_NAME}
+#quantum floatingip-create ${EXTERNAL_NETWORK_NAME}
