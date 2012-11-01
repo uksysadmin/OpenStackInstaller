b2496500b385edcdbac0866f66ca8a7e928d0226
uksysadmin <kevin@linuxservices.co.uk>
2012-11-01 15:02:56 +0000

    added install logging

--- install-folsom.sh
+++ install-folsom.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Run the installations
-./folsom-common.sh
-./folsom-keystone.sh
-./folsom-glance.sh
-./folsom-quantum.sh
-./folsom-cinder.sh
-./folsom-nova.sh
-./folsom-horizon.sh
+./folsom-common.sh 2>&1 >> /tmp/install.log
+./folsom-keystone.sh 2>&1 >> /tmp/install.log
+./folsom-glance.sh 2>&1 >> /tmp/install.log
+./folsom-quantum.sh 2>&1 >> /tmp/install.log
+./folsom-cinder.sh 2>&1 >> /tmp/install.log
+./folsom-nova.sh 2>&1 >> /tmp/install.log
+./folsom-horizon.sh 2>&1 >> /tmp/install.log
 
 # Creates a demo network for the admin tenant
 # Hard coded IPs in script which gets pulled by git for time being
 if [[ -f adminrc ]]
 then
 	. adminrc
-	./folsom-quantum-createnetwork.sh
+	./folsom-quantum-createnetwork.sh 2>&1 >> /tmp/install.log
 fi
