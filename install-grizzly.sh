7d673e9952b23c47af69bc3366bb3391432960e5
Kevin Jackson <kevin@linuxservices.co.uk>
2013-01-28 09:55:15 +0000

    Not using Quantum for the time being

--- install-grizzly.sh
+++ install-grizzly.sh
@@ -7,22 +7,22 @@ case $1 in
 		./install-mysql.sh 2>&1 >> /tmp/install.log
 		./install-keystone.sh 2>&1 >> /tmp/install.log
 		./install-glance.sh 2>&1 >> /tmp/install.log
-		./install-quantum.sh 2>&1 >> /tmp/install.log
+		# ./install-quantum.sh 2>&1 >> /tmp/install.log
 		./install-cinder.sh 2>&1 >> /tmp/install.log
 		./install-nova.sh 2>&1 >> /tmp/install.log
 		./install-horizon.sh 2>&1 >> /tmp/install.log
 		# Creates a demo network for the admin tenant
 		# Hard coded IPs in script which gets pulled by git for time being
-		if [[ -f adminrc ]]
-		then
-			. adminrc
-			./install-quantum-createnetwork.sh 2>&1 >> /tmp/install.log
-		fi
+		#if [[ -f adminrc ]]
+		#then
+		#	. adminrc
+		#	./install-quantum-createnetwork.sh 2>&1 >> /tmp/install.log
+		#fi
 		;;
 	"compute")
 		# Run the installations
 		./install-common.sh 2>&1 >> /tmp/install.log
-		./install-quantum-agent.sh 2>&1 >> /tmp/install.log
+		# ./install-quantum-agent.sh 2>&1 >> /tmp/install.log
 		./install-compute.sh 2>&1 >> /tmp/install.log
 		;;
 	*)
